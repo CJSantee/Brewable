b245cbb659f50e4dc3819acc2377818d5499eef8
CJSantee <santeedev@gmail.com>
2021-10-25 12:13:54 -0500

    Added flavor description

---
+++ frontend/src/Descriptions.js
@@ -0,0 +1,59 @@
+module.exports = {
+    flavor: "Flavor represents the coffee's principal character, "+
+    "the \"mid-range\" notes, in between the first impressions given by the "+
+    "coffee's first aroma and acidity to its final aftertaste. It is a "+
+    "combined impression of all gustatory (taste bud) sensations "+
+    "and retro-nasal aromas that go from the mouth to nose. The score "+
+    "given for Flavor should account for the intensity, quality and "+
+    "complexity of its combined taste and aroma, experienced when "+
+    "the coffee is slurped into the mouth vigorously so as to involve the "+
+    "entire palate in the evaluation.",
+    acidity: "Acidity is often described as \"brightness\" when favorable "+
+    "or \"sour\" when unfavorable. At its best, acidity contributes to a "+
+    "coffee's liveliness, sweetness, and fresh- fruit character and is "+
+    "almost immediately experienced and evaluated when the coffee is "+
+    "first slurped into the mouth. Acidity that is overly intense or "+
+    "dominating may be unpleasant, however, and excessive acidity "+
+    "may not be appropriate to the flavor profile of the sample. The "+
+    "final score marked on the horizontal tick-mark scale should reflect "+
+    "the panelist’s perceived quality for the Acidity relative to the "+
+    "expected flavor profile based on origin characteristics and/or "+
+    "other factors (degree of roast, intended use, etc.). Coffees "+
+    "expected to be high in Acidity, such as a Kenya coffee, or coffees "+
+    "expected to be low in Acidity, such as a Sumatra coffee, can "+
+    "receive equally high preference scores although their intensity "+
+    "rankings will be quite different.",
+    aroma: "The aromatic aspects include Fragrance "+
+    "(defined as the smell of the ground coffee when still dry) and "+
+    "Aroma (the smell of the coffee when infused with hot water). One "+
+    "can evaluate this at three distinct steps in the cupping process: (1) "+
+    "sniffing the grounds placed into the cup before pouring water "+
+    "onto the coffee; (2) sniffing the aromas released while breaking "+
+    "the crust; and (3) sniffing the aromas released as the coffee "+
+    "steeps. Specific aromas can be noted under “qualities” and the "+
+    "intensity of the dry, break, and wet aroma aspects noted on the 5- "+
+    "point vertical scales. The score finally given should reflect the "+
+    "preference of all three aspects of a sample’s Fragrance/Aroma.",
+    body: "The quality of Body is based upon the tactile feeling of the "+
+    "liquid in the mouth, especially as perceived between the tongue "+
+    "and roof of the mouth. Most samples with heavy Body may also "+
+    "receive a high score in terms of quality due to the presence of "+
+    "brew colloids and sucrose. Some samples with lighter Body may "+
+    "also have a pleasant feeling in the mouth, however. Coffees "+
+    "expected to be high in Body, such as a Sumatra coffee, or coffees "+
+    "expected to be low in Body, such as a Mexican coffee, can receive "+
+    "equally high preference scores although their intensity rankings "+
+    "will be quite different.",
+    sweetness: "Sweetness refers to a pleasing fullness of flavor as "+
+    "well as any obvious sweetness and its perception is the result of "+
+    "the presence of certain carbohydrates. The opposite of sweetness "+
+    "in this context is sour, astringency or “green” flavors. This quality "+
+    "may not be directly perceived as in sucrose-laden products such "+
+    "as soft drinks, but will affect other flavor attributes. 2 points are "+
+    "awarded for each cup displaying this attribute for a maximum "+
+    "score of 10 points.",
+    aftertaste: "Aftertaste is defined as the length of positive flavor "+
+    "(taste and aroma) qualities emanating from the back of the palate "+
+    "and remaining after the coffee is expectorated or swallowed. If the "+
+    "aftertaste were short or unpleasant, a lower score would be given."
+};
--- frontend/src/pages/InfoPage.js
+++ frontend/src/pages/InfoPage.js
@@ -9,15 +9,42 @@ import { useTheme } from '@react-navigation/native';
 // Component Imports
 import Header from '../components/Header';
 
+import { acidity, aftertaste, aroma, body, flavor, sweetness } from '../Descriptions';
+
 const InfoPage = ({ route, navigation }) => {
     const { topic } = route.params; // Topic to display info on 
     const { colors } = useTheme(); // Color theme
+
+    let description;
+    switch (topic) {
+        case 'Flavor':
+            description = flavor;
+            break;
+        case 'Acidity':
+            description = acidity;
+            break;
+        case 'Aroma':
+            description = aroma;
+            break;
+        case 'Body':
+            description = body;
+            break;
+        case 'Sweetness':
+            description = sweetness;
+            break;
+        case 'Aftertaste':
+            description = aftertaste;
+            break;
+        default: 
+            description = "";
+    }
 
     return (
         <View style={styles.container}>
             <Header title="About" leftText="Back" leftOnPress={() => navigation.goBack()} leftChevron={true}/>
             <View style={{...styles.card, backgroundColor: colors.card}}>
                 <Text style={styles.title}>{topic}</Text>
+                <Text style={styles.description}>{description}</Text>
             </View>
         </View>
     );
@@ -41,5 +68,9 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 22,
         margin: 5
+    },
+    description: {
+        fontSize: 16,
+        marginHorizontal: 15
     }
 });
