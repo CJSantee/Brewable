fabe678b4dd3f58eb3feef5f68649ce04b4c0d0d
CJSantee <santeedev@gmail.com>
2021-10-27 00:31:30 -0500

    Redesigned Brew.js

--- frontend/src/Brew.js
+++ frontend/src/Brew.js
@@ -7,11 +7,10 @@ import {
 } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faTint, faFire, faStopwatch, faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faTint, faFire, faStopwatch, faHeart as faHeartSolid, faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faStar } from '@fortawesome/free-regular-svg-icons';
 import CoffeeBean from '../assets/coffeeBean.svg';
 
-import { HoldItem } from 'react-native-hold-menu';
 import TastingWheel from './components/TastingWheel';
 
 const Brew = ({ brew, menuItems, onFavorite, navigation }) => {
@@ -21,16 +20,6 @@ const Brew = ({ brew, menuItems, onFavorite, navigation }) => {
     const options = { weekdate: 'long', year: 'numeric', month: 'long', day: 'numeric' }; // Options for date string
     var brew_date = new Date(brew.date);
     let date_string = brew_date.toLocaleDateString('en-US', options);
-    
-    // const menuItems = [
-    //     { 
-    //         text: 'Edit', 
-    //         icon: 'edit',
-    //         onPress:  () => navigation.navigate("EditBrew", { brew_id: brew.id })
-    //     },
-    //     { text: 'Share', icon: 'share', onPress: () => {} },
-    //     { text: 'Delete', icon: 'trash', isDestructive: true, onPress: () => onDelete(brew.id) },
-    // ];
 
     // Add Favorite
     function toggleFavorite(e) {
@@ -39,42 +28,55 @@ const Brew = ({ brew, menuItems, onFavorite, navigation }) => {
     }
 
     return (
-        // <HoldItem items={menuItems} key={brew.id}>
-            <TouchableWithoutFeedback onPress={() => navigation.navigate("DisplayBrew", { brew_id: brew.id })}>
-                <View style={{...styles.brew, backgroundColor: colors.card, borderColor: colors.border}}>
+        <TouchableWithoutFeedback onPress={() => navigation.navigate("DisplayBrew", { brew_id: brew.id })}>
+            <View style={{...styles.brew, backgroundColor: colors.card, borderColor: colors.border}}>
+                <View style={styles.wheel}>
+                    <TastingWheel displayText={false} width="150" height="150" values={[brew.body, brew.aftertaste, brew.sweetness, brew.aroma, brew.flavor, brew.acidity]}/>
+                    {false&&<Text style={{top: -10, fontWeight: 'bold'}}>{brew.roaster}</Text>}
+                    {false&&<Text style={{top: -5}}>{brew.region}</Text>}
+                </View>
+                <View style={styles.leftItems}>
                     <View style={styles.cardItem}>
-                        <Text style={{fontWeight: 'bold', fontSize: 18}}>{brew.brew_method}</Text>
+                        <Text style={styles.title}>{brew.brew_method}</Text>
                     </View>
                     <View style={styles.cardItem}>
-                        <FontAwesomeIcon size={20} icon={faTint} color="#0069A7"/>
+                        <FontAwesomeIcon size={22} icon={faTint} color="#0069A7"/>
                         <Text style={styles.value}>{brew.water}</Text>
                         <Text>{brew.water_unit}</Text>
                     </View>
                     <View style={styles.cardItem}>
-                        <CoffeeBean width={20} height={20} style={{color: "#714B33"}}/>
+                        <CoffeeBean width={22} height={22} style={{color: "#714B33"}}/>
                         <Text style={styles.value}>{brew.coffee}</Text>
                         <Text>{brew.coffee_unit}</Text>
                     </View>
                     <View style={styles.cardItem}>
-                        <FontAwesomeIcon size={20} icon={faFire} color="#EB811E"/>
+                        <FontAwesomeIcon size={22} icon={faFire} color="#EB811E"/>
                         <Text style={styles.value}>{brew.temperature}</Text>
                         <Text>°{brew.temp_unit}</Text>
                     </View>
                     <View style={styles.cardItem}>
-                        <FontAwesomeIcon size={20} icon={faStopwatch} color="#4D814B"/>
+                        <FontAwesomeIcon size={22} icon={faStopwatch} color="#4D814B"/>
                         <Text style={styles.value}>{brew.time}</Text>
                     </View>
-                    <TouchableWithoutFeedback onPress={toggleFavorite}>
-                        <View style={styles.favorite}>
-                            <FontAwesomeIcon icon={brew.favorite?faHeartSolid:faHeart} size={22} color={brew.favorite?"#a00": colors.placeholder}/>
-                            <Text style={{marginLeft: 5, fontWeight: 'bold', fontSize: 16}}>{brew.rating}/5</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
-                    <TastingWheel style={styles.wheel} displayText={false} width="125" height="125" values={[brew.body, brew.aftertaste, brew.sweetness, brew.aroma, brew.flavor, brew.acidity]}/>
-                    <Text style={styles.date}>{date_string}</Text>
+                    
+                    <View style={styles.rating}>
+                        {Array(brew.rating).fill().map((_, idx)=>idx).map((value) => (
+                            <FontAwesomeIcon icon={faStarSolid} size={18} color={'rgb(255,149,67)'}/>
+                        ))}
+                        {Array(5-brew.rating).fill().map((_, idx)=>idx).map((value) => (
+                            <FontAwesomeIcon icon={faStar} size={18} color={'rgb(255,149,67)'}/>
+                        ))}
+                    </View>
                 </View>
-            </TouchableWithoutFeedback>
-        // </HoldItem>
+                <TouchableWithoutFeedback onPress={toggleFavorite}>
+                    <View style={styles.favorite}>
+                        <FontAwesomeIcon icon={brew.favorite?faHeartSolid:faHeart} size={18} color={brew.favorite?"#a00": colors.placeholder}/>
+                    </View>
+                </TouchableWithoutFeedback>
+                
+                <Text style={styles.date}>{date_string}</Text>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 
@@ -86,7 +88,7 @@ const styles = StyleSheet.create({
         margin: 10,
     },
     title: {
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: 'bold',
     },
     subtitle: {
@@ -94,22 +96,25 @@ const styles = StyleSheet.create({
     },
     brew: {
         flex: 1,
+        flexDirection: 'column',
         marginTop: 5,
         marginHorizontal: 10,
-        borderWidth: 0.5,
+        borderWidth: 0.8,
         height: 250,
         borderRadius: 10,
         paddingHorizontal: 10,
-        paddingVertical: 5,
         overflow: 'hidden'
     },
-    wheel: {
-        width: "100%",
-        height: "100%",
+    leftItems: {
+        height: '100%',
+        flexDirection: 'column',
         position: 'absolute',
-        top: -10,
-        right: -5,
-        alignItems: 'flex-end',
+        left: 10,
+        top: 5
+    },  
+    wheel: {
+        alignSelf: 'flex-end',
+        alignItems: 'center'
     },
     cardItem: {
         flexDirection: 'row',
@@ -117,25 +122,30 @@ const styles = StyleSheet.create({
         marginBottom: 8,
     },
     value: {
-        fontSize: 16,
+        fontSize: 18,
         marginLeft: 2,
         marginRight: 1,
     },
     date: {
         position: 'absolute',
-        bottom: 8, 
-        right: 10
+        bottom: 10, 
+        right: 10,
+        fontSize: 16
     },  
     notes: {
         overflow: 'hidden',
         width: "55%",
     },
+    rating: {
+        position: 'absolute',
+        bottom: 15,
+        flexDirection: 'row'
+    },
     favorite: {
         position: 'absolute',
         flexDirection: 'row',
-        alignItems: 'center',
-        bottom: 8,
-        left: 10,
-        zIndex: 1,
+        top: 10,
+        right: 10,
+        zIndex: 1
     }
 });
