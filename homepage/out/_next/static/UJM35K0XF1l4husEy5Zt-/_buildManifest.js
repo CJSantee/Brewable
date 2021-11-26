a11c0d0242fb0afb09ec093e014aa612a925746e
CJSantee <santeedev@gmail.com>
2021-11-25 22:13:30 -0600

    Initial out subtree commit gh-pages

---
+++ homepage/out/_next/static/UJM35K0XF1l4husEy5Zt-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9a62d2f6dae43c65.css","static/chunks/pages/index-f0723edaa2dfbab1.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
