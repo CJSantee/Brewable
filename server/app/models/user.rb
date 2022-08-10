d6ae5a49af6ed6828b55c5d3b09f5adb39499c3c
CJSantee <santeedev@gmail.com>
2022-08-09 21:22:16 -0500

    Moved /server to Brewable-Server repo

--- server/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-	has_secure_password 
-	
-	validates :email, uniqueness: true
-	validates :phone, uniqueness: true, :allow_blank => true
-end
--- server/config/environment.rb
+++
@@ -1,9 +0,0 @@
-# Load the Rails application.
-require_relative "application"
-
-# Load local ENV variables
-env = File.join(Rails.root, 'config', 'set_env.rb')
-load(env) if File.exist?(env)
-
-# Initialize the Rails application.
-Rails.application.initialize!
