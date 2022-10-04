f22f3952aa3f0e94c81bec33f4c9dff8c64554a1
CJSantee <santeedev@gmail.com>
2022-10-03 20:30:43 -0500

    Inital Navbar

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,88 +1,71 @@
+// React
 import { useState } from "react";
-
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// Assets
+import icon from "../assets/Brewable-Icon_1024x1024.png";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+// Components
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import SignInModal from "./SignInModal";
 import SignUpModal from "./SignUpModal";
+// Hooks
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   const [showSignIn, setShowSignIn] = useState(false);
   const [showSignUp, setShowSignUp] = useState(false);
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
 
   const { auth, logout } = useAuth();
   const navigate = useNavigate();
 
+  const closeWith = (fxn) => {
+    setShowOffcanvas(false);
+    fxn();
+  };
+
   return (
     <>
-      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-        <Container fluid className='px-5'>
-          <Navbar.Brand onClick={() => navigate("/")}>Brewable</Navbar.Brand>
+      <nav className='navbar box-shadow bg-white justify-content-between justify-content-sm-start'>
+        <a className='navbar-brand ms-3' href='/'>
+          <img src={icon} width='55' height='55' alt='Brewable Icon' />
+        </a>
+        <form action='' className='form-inline'>
+          <input
+            type='text'
+            className='form-control rounded-pill bg-light'
+            placeholder='Search Brewable...'
+          />
+        </form>
+        <button
+          onClick={() => setShowOffcanvas(true)}
+          className='btn btn-outline-secondary d-flex d-sm-none me-3'
+        >
+          <FontAwesomeIcon icon={faBars} size='xl' />
+        </button>
+        <div className='d-none d-sm-flex ms-auto mx-3'>
           {auth.user ? (
             <>
-              <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-              <Navbar.Collapse id='responsive-navbar-nav'>
-                <Nav className='me-auto'>
-                  <Nav.Link onClick={() => navigate("/beans")}>Beans</Nav.Link>
-                  <Nav.Link onClick={() => navigate("/brews")}>Brews</Nav.Link>
-                </Nav>
-                <Nav className='align-items-center'>
-                  <Nav.Item>
-                    <button
-                      className='btn btn-sm btn-outline-light'
-                      onClick={() => navigate("/new")}
-                    >
-                      New
-                    </button>
-                  </Nav.Item>
-                  <NavDropdown
-                    title={auth.user.first_name + " " + auth.user.last_name}
-                    className='mr-2'
-                    id='collasible-nav-dropdown'
-                  >
-                    <NavDropdown.Item onClick={() => navigate("/profile")}>
-                      Profile
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href='#action/3.2'>
-                      My Beans
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href='#action/3.3'>
-                      My Brews
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href='#action/3.4'>
-                      Settings
-                    </NavDropdown.Item>
-                    <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-              </Navbar.Collapse>
+              <button onClick={logout} className='btn btn-outline-primary'>
+                Log Out
+              </button>
             </>
           ) : (
-            <Nav>
-              <Nav.Item className='me-2'>
-                <Nav.Link
-                  className='text-light'
-                  onClick={() => setShowSignIn(true)}
-                >
-                  Sign In
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <button
-                  className='btn btn-outline-light'
-                  onClick={() => setShowSignUp(true)}
-                >
-                  Sign Up
-                </button>
-              </Nav.Item>
-            </Nav>
+            <>
+              <button onClick={() => setShowSignIn(true)} className='btn mr-2'>
+                Sign In
+              </button>
+              <button
+                onClick={() => setShowSignUp(true)}
+                className='btn btn-outline-primary'
+              >
+                Sign Up
+              </button>
+            </>
           )}
-        </Container>
-      </Navbar>
+        </div>
+      </nav>
 
       <SignInModal
         show={showSignIn}
@@ -94,6 +77,44 @@ export default function Header() {
         setShow={setShowSignUp}
         showSignIn={() => setShowSignIn(true)}
       />
+      <Offcanvas
+        show={showOffcanvas}
+        onHide={() => setShowOffcanvas(false)}
+        responsive='sm'
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Brewable</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div className='d-flex flex-column'>
+            {auth.user ? (
+              <>
+                <button
+                  onClick={() => closeWith(logout)}
+                  className='btn btn-outline-primary'
+                >
+                  Log Out
+                </button>
+              </>
+            ) : (
+              <>
+                <button
+                  onClick={() => closeWith(() => setShowSignIn(true))}
+                  className='btn mr-2'
+                >
+                  Sign In
+                </button>
+                <button
+                  onClick={() => closeWith(() => setShowSignUp(true))}
+                  className='btn btn-outline-primary'
+                >
+                  Sign Up
+                </button>
+              </>
+            )}
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 }
--- web/src/index.js
+++ web/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 
-import "./custom.scss";
-import "./index.css";
+import "./assets/_common/scss/styles.scss";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
